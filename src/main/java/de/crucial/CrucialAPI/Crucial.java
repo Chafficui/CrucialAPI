04359672d6f6df13e6b6e44c7a7d2b8edd2aeab1
Chaffic <felixbeinssen@gmail.com>
2020-12-10 09:40:24 +0100

    Small bugfixes

--- src/main/java/de/crucial/CrucialAPI/Crucial.java
+++ src/main/java/de/crucial/CrucialAPI/Crucial.java
@@ -3,6 +3,7 @@ package de.crucial.CrucialAPI;
 import de.crucial.CrucialAPI.API.Server;
 import de.crucial.CrucialAPI.API.Updater;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,22 +14,25 @@ import java.util.logging.Logger;
 
 public class Crucial {
 
-    private static final Main plugin = Main.getPlugin(Main.class);
-    private static final Logger logger = Server.getLogger("CrucialAPI");
+    private static final Main PLUGIN = Main.getPlugin(Main.class);
+    private static final Logger LOGGER = Server.getLogger("CrucialAPI");
 
     public static void getUpdate(){
-        Updater.update(plugin, 86380, plugin.getFileH());
+        Updater.update(PLUGIN, 86380, PLUGIN.getFileH());
     }
 
-    public static void getVersion(String version){
-        try {
-            URL website = new URL("https://github.com/Chafficui/CrucialAPI/releases/download/v" + version + "/CrucialAPI-v" + version + ".jar");
-            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-            FileOutputStream fos = new FileOutputStream("plugins/CrucialAPI.jar");
-            fos.getChannel().transferFrom(rbc, 0L, Long.MAX_VALUE);
-        } catch (IOException e) {
-            logger.severe("Error 28: Failed to update CrucialAPI");
+    public static void getVersion(String version, JavaPlugin plugin){
+        if(version != PLUGIN.getDescription().getVersion()) {
+            try {
+                URL website = new URL("https://github.com/Chafficui/CrucialAPI/releases/download/v" + version + "/CrucialAPI-v" + version + ".jar");
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream("plugins/CrucialAPI.jar");
+                fos.getChannel().transferFrom(rbc, 0L, Long.MAX_VALUE);
+            } catch (IOException e) {
+                LOGGER.severe("Error 28: Failed to update CrucialAPI");
+            }
+            Bukkit.getPluginManager().disablePlugin(PLUGIN);
+            Bukkit.getPluginManager().disablePlugin(plugin);
         }
-        Bukkit.getPluginManager().disablePlugin(plugin);
     }
 }
