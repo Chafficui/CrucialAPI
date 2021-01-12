277ad079903a0031240c372f9817f8f9e0ecdaaf
Chaffic <felixbeinssen@gmail.com>
2021-01-12 10:18:08 +0100

    Server.getVersion is deprecated and replaced by Server.checkVersion

--- src/main/java/de/crucial/CrucialAPI/API/Server.java
+++ src/main/java/de/crucial/CrucialAPI/API/Server.java
@@ -6,7 +6,21 @@ import java.util.logging.Logger;
 
 public class Server {
 
+    /**
+     * Replaced
+     * Use checkVersion(String[] valid Version)
+     */
+    @Deprecated
     public static boolean getVersion(String[] validVersions) {
+        for (String validVersion:validVersions) {
+            if(Bukkit.getVersion().contains(validVersion)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkVersion(String[] validVersions) {
         for (String validVersion:validVersions) {
             if(Bukkit.getVersion().contains(validVersion)){
                 return true;
