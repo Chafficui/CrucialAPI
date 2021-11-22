7705dca10f0979d99620dbd47e66ca7c7965f2f9
Beinßen, Felix <felix.beinssen@student.hs-anhalt.de>
2021-11-22 16:37:12 +0100

    Deprecated static access to Stats.java Created new way to connect to BStats

--- src/main/java/io/github/chafficui/CrucialAPI/Utils/Stats.java
+++ src/main/java/io/github/chafficui/CrucialAPI/Utils/Stats.java
@@ -1,32 +1,53 @@
 package io.github.chafficui.CrucialAPI.Utils;
 
-import io.github.chafficui.CrucialAPI.exceptions.CrucialException;
 import io.github.chafficui.CrucialAPI.Utils.api.BStats;
+import io.github.chafficui.CrucialAPI.exceptions.CrucialException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * An implementation of bstats.org pie charts
  */
 public class Stats {
-    private static BStats metrics;
+    private static BStats METRICS;
+    private final BStats metrics;
 
     /**
-     * @param plugin is the main class of your project
-     * @param pluginID The unique ID you get at bstats website
+     * @param plugin   is the main class of your project
+     * @param bstatsID The unique ID you get at bstats website
      */
-    public  static void setMetrics(JavaPlugin plugin, int pluginID){
-        metrics = new BStats(plugin, pluginID);
+    public Stats(JavaPlugin plugin, int bstatsID) {
+        this.metrics = new BStats(plugin, bstatsID);
     }
-
 
     /**
      * @param name of the pie chart
      * @param data that will be processed in the pie chart
+     */
+    public void sendPieChart(String name, String data) {
+        metrics.addCustomChart(new BStats.SimplePie(name, () -> data));
+    }
+
+    /**
+     * @param plugin   is the main class of your project
+     * @param pluginID The unique ID you get at bstats website
+     * @deprecated Use the Stats Object instead
+     */
+    @Deprecated
+    public static void setMetrics(JavaPlugin plugin, int pluginID) {
+        METRICS = new BStats(plugin, pluginID);
+    }
+
+
+    /**
+     * @deprecated Use the Stats Object instead
+     * @param name of the pie chart
+     * @param data that will be processed in the pie chart
      * @throws io.github.chafficui.CrucialAPI.exceptions.CrucialException if there was no metrics created before.
      */
+    @Deprecated
     public static void addPieChart(String name, String data) throws CrucialException {
-        if(metrics != null){
-            metrics.addCustomChart(new BStats.SimplePie(name, () -> data));
+        if (METRICS != null) {
+            METRICS.addCustomChart(new BStats.SimplePie(name, () -> data));
         } else {
             throw new CrucialException(4);
         }
