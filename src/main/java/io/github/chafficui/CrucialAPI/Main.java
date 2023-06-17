bcb43fe7cab9a1141ba9201ad4b61fe36aa50b77
Beinßen, Felix <felix.beinssen@student.hs-anhalt.de>
2023-06-17 16:29:49 +0200

    clean: removed unused imports

--- src/main/java/io/github/chafficui/CrucialAPI/Main.java
+++ src/main/java/io/github/chafficui/CrucialAPI/Main.java
@@ -10,12 +10,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 
 public final class Main extends JavaPlugin {
     private final String version = getDescription().getVersion();
