3cbf95e01feb395ee324616c1740e90de51d5212
Beinßen, Felix <felix.beinssen@student.hs-anhalt.de>
2023-06-17 17:03:03 +0200

    feat: added simple itemPrefab design

---
+++ src/main/java/io/github/chafficui/CrucialAPI/Utils/player/inventory/prefabs/InventoryItemPrefab.java
@@ -0,0 +1,7 @@
+package io.github.chafficui.CrucialAPI.Utils.player.inventory.prefabs;
+
+import io.github.chafficui.CrucialAPI.Utils.player.inventory.InventoryItem;
+
+public interface InventoryItemPrefab {
+    public InventoryItem[] getItems();
+}
